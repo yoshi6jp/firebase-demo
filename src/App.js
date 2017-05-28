559adce471d31a72c424c3b9fa7d064890af801f
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2017-05-28 09:35:59 +0900

    step1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+<div className="container">
+  <form>
+    <div className="form-group row">
+      <label htmlFor="date" className="col-2 col-form-label">測定日</label>
+      <div className="col-10">
+        <input name="date" 
+         className="form-control" type="date" id="date" />
       </div>
+    </div>
+    <div className="form-group row">
+      <label htmlFor="weight" className="col-2 col-form-label">体重</label>
+      <div className="col-10">
+        <input name="weight" 
+         className="form-control" type="number" id="weight" />
+      </div>
+    </div>
+    <div className='row'>
+      <div className='offset-2 col-10'>
+        <button className='btn btn-primary'>登録</button>
+      </div>
+    </div>
+  </form>
+</div>
     );
   }
 }
