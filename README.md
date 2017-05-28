cf196338df9f6bae900bf18aa3896c989e1fc69b
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2017-05-28 09:55:02 +0900

    step2

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # 体重記録アプリのサンプル
-- 測定日と体重を記録する
+- 測定日と体重を記録する
 
-## [bootstrap](https://v4-alpha.getbootstrap.com/)
+## [bootstrap](https://v4-alpha.getbootstrap.com/)の追加
 
 - cdnの追加
 - [form](https://v4-alpha.getbootstrap.com/components/forms/)の作成
 - [HTML to JSX](http://magic.reactjs.net/htmltojsx.htm)
 
+## 入力値の取得
+
+- https://facebook.github.io/react/
+  - onSubmit
+  - onChange
+  - value
+- Chart Componetの追加
--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      items: [],
+      date: '',
+      weight: ''
+    };
+  }
   render() {
     return (
 <div className="container">
-  <form>
+  <form onSubmit={this.handleSubmit}>
     <div className="form-group row">
       <label htmlFor="date" className="col-2 col-form-label">測定日</label>
       <div className="col-10">
         <input name="date" 
+         onChange={this.handleChange} 
+         value={this.state.date}
          className="form-control" type="date" id="date" />
       </div>
     </div>
@@ -17,6 +29,8 @@ class App extends Component {
       <label htmlFor="weight" className="col-2 col-form-label">体重</label>
       <div className="col-10">
         <input name="weight" 
+         onChange={this.handleChange} 
+         value={this.state.weight}
          className="form-control" type="number" id="weight" />
       </div>
     </div>
@@ -26,9 +40,41 @@ class App extends Component {
       </div>
     </div>
   </form>
+  <Chart items={this.state.items}/>
+
 </div>
     );
   }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    let {weight, date} = this.state
+    if(!weight || !date){
+      return
+    }
+    let newItem = {
+      date,
+      weight
+    }
+    this.setState((prevState) => ({
+      items: prevState.items.concat(newItem),
+      weight: '',
+      date: ''
+    }));
+  }
 }
 
+class Chart extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.date}>{item.weight}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 export default App;
